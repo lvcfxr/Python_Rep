8bc5e0acb3c5f51300b20beb139b82db879c19a0
Oleg <olgznch@gmail.com>
2024-02-19 22:05:22 +0300

    Oleg commit

--- main.py
+++ main.py
@@ -3,10 +3,6 @@ import functools
 print("hello")
 print("World")
 
-def factorial(n):
-    result = 1
-    for num in range(1, n + 1):
-        result *= num
-    return result
-print(factorial(5))
+def sum(a, b):
+    return a + b
 
